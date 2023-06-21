75bf98e63e2592fe2714a0f6706910a8755891f3
jiwookki <jiwookki@gmail.com>
2023-06-21 03:17:29 -0400

    created RectangleRenderer
    untested

--- renderer.py
+++ renderer.py
@@ -39,3 +39,32 @@ class RectRenderer(Renderer):
     
     def render(self, screen):
         pygame.draw.rect(screen, self.color, self.rect, width=1)
+
+
+
+class RectangleRenderer(Renderer):
+    def __init__(self, point1, point2, width, color):
+        '''
+        Used to define a rectangle that can be rotated. 
+        point1 and point2 are Vector2 objects that will be used to define one side of the rectangle. 
+        The distance between point1 and point2 is the length of the rectangle. 
+        From this information, point3 and point4 will be generated, at distance 'width'
+        away from point1 and point2, and at 90 degree angle from point1 and point2 side. 
+        '''
+        super().__init__(point1[0], point1[1], color)
+        self.points = [
+            point1,
+            point2,
+            ((point2-point1).normalize().rotate(90) * width) + point1,
+            ((point2-point1).normalize().rotate(90) * width) + point2
+        ]
+    
+    def update_position(self, x, y):
+        
+        for point in self.points:
+            point.x += x
+            point.y += y
+            self.points[self.points.index(point)] = point
+
+    def render(self, screen):
+        pygame.draw.polygon(screen, self.color, self.points)
